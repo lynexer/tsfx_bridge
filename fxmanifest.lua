fe0ccfd77853855c3f6d6496e6bd73c9c4a573ba
Kyle Graham <kylegraham05@gmail.com>
2025-11-29 10:28:33 -0800

    feat: create fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+shared_scripts {
+    'dist/shared/**/*.js'
+}
+
+server_scripts {
+    'dist/server/**/*.js'
+}
+
+client_scripts {
+    'dist/client/**/*.js'
+}
