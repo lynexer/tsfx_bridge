287aa1be2707cd2433ba01714798543b3e6fd8e1
Kyle Graham <kylegraham05@gmail.com>
2025-11-30 08:06:50 -0800

    chore(server): register player module events

--- src/server/modules/player.ts
+++ src/server/modules/player.ts
@@ -1,6 +1,8 @@
-import { FrameworkAdapter } from "@server/adapters/framework/base";
-import { BridgeModule } from "./base";
-import { Player } from "@shared/types/player.types";
+import type { FrameworkAdapter } from '@server/adapters/framework/base';
+import { eventEmitter } from '@server/events/emitter';
+import { BridgeEvents } from '@shared/constants/events';
+import type { Player } from '@shared/types/player.types';
+import { BridgeModule } from './base';
 
 export class PlayerModule extends BridgeModule {
     private adapter: FrameworkAdapter;
@@ -15,7 +17,13 @@ export class PlayerModule extends BridgeModule {
     }
 
     registerEvents(): void {
-        // TODO
+        this.adapter.onPlayerLoaded((source, player) => {
+            eventEmitter.emit(BridgeEvents.PLAYER_LOADED, source, player);
+        });
+
+        this.adapter.onPlayerUnloaded((source) => {
+            eventEmitter.emit(BridgeEvents.PLAYER_UNLOADED, source);
+        });
     }
 
     getPlayer(source: number): Player | null {
