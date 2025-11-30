732bb27f26ac251ff10e1abd6e6e028d3758b4c9
Kyle Graham <kylegraham05@gmail.com>
2025-11-29 23:14:16 -0800

    chore(server): add qbx adapter detection

--- src/server/detection/framework.ts
+++ src/server/detection/framework.ts
@@ -1,9 +1,14 @@
 import type { FrameworkAdapter } from '@server/adapters/framework/base';
 import { QBAdapter } from '@server/adapters/framework/qb';
+import { QBXAdapter } from '@server/adapters/framework/qbx';
 
 export const detectFramework = (): FrameworkAdapter => {
     if (GetResourceState('qb-core') === 'started') {
         return new QBAdapter();
+    }
+
+    if (GetResourceState('qbx_core') === 'started') {
+        return new QBXAdapter();
     }
 
     throw new Error('No supported framework detected!');
